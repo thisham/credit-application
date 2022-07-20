2ce56dc2f1fdac6cd09b055f7d5a41cef6bf0f37
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-07-20 14:22:27 +0700

    feat: add router for form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginPage from './app/login/pages';
 import HomePage from './app/home/pages';
+import CreditForm from './app/credit-add/pages';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +13,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginPage} />
         <Stack.Screen name="Home" component={HomePage} />
-        {/* <Stack.Screen name="Login" component={LoginPage} /> */}
+        <Stack.Screen name="CreditForm" component={CreditForm} />
       </Stack.Navigator>
     </NavigationContainer>
   );
