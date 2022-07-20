0742e47879e4ba01d81f8daf5ac51d9ca9f35697
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-07-20 12:00:49 +0700

    feat: add home page

---
+++ src/app/home/styles/PageStylesheet.ts
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    paddingTop: 40,
+  },
+  cardContainer: {
+    width: '80%',
+    marginTop: 40,
+    paddingHorizontal: '5%',
+    paddingVertical: '5%',
+    marginHorizontal: '10%',
+  },
+  titleStyle: {
+    marginBottom: 20,
+  },
+  usernameStyle: {
+    borderColor: '#acacac',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    marginBottom: 5,
+  },
+});
+
+export default styles;
